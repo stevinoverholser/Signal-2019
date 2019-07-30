7429dc75d473234caaa369719d3c9d79d38103d6
stevinoverholser <stevin.overholser@sendgrid.com>
2019-07-30 13:18:23 -0600

    Update SG_Mail_Send.js

--- SG_Mail_Send.js
+++ SG_Mail_Send.js
@@ -5,7 +5,10 @@ exports.handler = function(context, event, callback) {
     var pnumber = event.pnumber;
     var email = event.email;
     console.log(fname + pnumber);
-    var payload = "{\"personalizations\": [{\"to\": [{\"email\": \"" + email + "\", \"name\": \"" + fname + "\"} ], \"subject\": \"Twilio app proof of concept\"} ], \"from\": {\"email\": \"austin@rhondajules.com\", \"name\": \"Stevin's Twilio APP!\"}, \"subject\": \"Proof of concept\", \"content\": [{\"type\": \"text\/html\", \"value\": \"<h3>Hello " + fname + ", Thanks for signing up!<\/h3> <p><a href=\\\"https:\/\/www.twilio.com\/\\\">Click here to get started<\/a><\/p>\"} ], \"categories\": [\"TwilAPP\"], \"custom_args\": {\"Phonenumber\" : \"" + pnumber + "\"} , \"tracking_settings\": {\"click_tracking\": {\"enable\": true }, \"open_tracking\": {\"enable\": true } } }";
+    var subject = "Twilio app proof of concept";
+    var from = "austin@stevin.world";
+    var html = "<h3>Hello " + fname + ", Thanks for signing up!<\/h3> <p><a href=\\\"https:\/\/www.twilio.com\/\\\">Click here to get started<\/a><\/p>";
+    var payload = "{\"personalizations\": [{\"to\": [{\"email\": \"" + email + "\", \"name\": \"" + fname + "\"} ], \"subject\": \"" + subject + "\"} ], \"from\": {\"email\": \"" + from + "\"}, \"subject\": \"" + subject + "\", \"content\": [{\"type\": \"text\/html\", \"value\": \""+ html + "\"} ], \"categories\": [\"TwilAPP\"], \"custom_args\": {\"Phonenumber\" : \"" + pnumber + "\"} , \"tracking_settings\": {\"click_tracking\": {\"enable\": true }, \"open_tracking\": {\"enable\": true } } }";
     console.log("Payload: " + payload);
     got.post(uri, {
     body: payload,
